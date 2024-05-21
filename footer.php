95dc665059314e57f9a38d166c9e1d169ea5bf2c
Job Njenga <90536181+Astro-no@users.noreply.github.com>
2024-05-21 13:32:33 +0300

    Copyright
    And ofcourse, the copyright reward goes to MERU UNIVERSITY OF SCIENCE AND TECHNOLOGY.

---
+++ footer.php
@@ -0,0 +1,11 @@
+// Copyright
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy;MERU UNIVERSITY OF SCIENCE AND TECHNOLOGY |<a href="#" target="_blank"  > Designed by : THE GROUP</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
